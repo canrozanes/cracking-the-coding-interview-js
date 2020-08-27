2a0047478d6f2f05e3704186ba06fc6e9a45cb74
Can Rozanes <37912128+canrozanes@users.noreply.github.com>
2020-08-26 21:51:33 -0400

    introduce typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,21 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    commonjs: true,
     es2020: true,
+    commonjs: true,
     jest: true,
   },
   extends: [
     'airbnb-base',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 11,
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
+  plugins: [
+    '@typescript-eslint',
+  ],
   rules: {
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
     'operator-assignment': 'off',
@@ -19,6 +24,47 @@ module.exports = {
     'max-classes-per-file': ['error', 2],
     'consistent-return': 'off',
     'no-param-reassign': 'off',
+    'no-unused-vars': 'off',
   },
-  ignorePatterns: ['*.config.js'],
+  ignorePatterns: ['*.config.js', '/built'],
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+
+        // typescript-eslint specific options
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+      plugins: ['@typescript-eslint'],
+      // If adding a typescript-eslint version of an existing ESLint rule,
+      // make sure to disable the ESLint rule here.
+      rules: {
+        // Add TypeScript specific rules (and turn off ESLint equivalents)
+        '@typescript-eslint/no-unused-vars': 'off',
+        'import/extensions': [
+          'error',
+          'ignorePackages',
+          {
+            js: 'never',
+            jsx: 'never',
+            ts: 'never',
+            tsx: 'never',
+          },
+        ],
+      },
+      settings: {
+        'import/resolver': {
+          node: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          },
+        },
+      },
+    },
+  ],
 };
--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@
 module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
+  preset: 'ts-jest',
 
   // Stop running tests after `n` failures
   // bail: 0,
@@ -74,7 +75,7 @@ module.exports = {
 
   // An array of file extensions your modules use
   moduleFileExtensions: [
-    'js',
+    'js','ts'
   ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
