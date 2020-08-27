d542cd81183fcac8acfa08edf59daf97c337d1df
Can Rozanes <37912128+canrozanes@users.noreply.github.com>
2020-08-27 10:06:23 -0400

    convert graph to typescript

---
+++ src/data-structures/graph/index.ts
@@ -0,0 +1,55 @@
+import Node from './node';
+
+class Graph<T> {
+  private _nodes: Node<T>[]
+
+  constructor() {
+    this._nodes = [];
+  }
+
+  get nodes() {
+    return this._nodes;
+  }
+
+  addNode(name: string, value?: T): void {
+    const hasNode = this.nodes.filter((node) => node.name === name).length > 0;
+    if (!hasNode) {
+      const newNode = new Node(name, value);
+      this.nodes.push(newNode);
+    }
+  }
+
+  hasNode(name: string) : boolean {
+    return this.nodes.filter((node) => node.name === name).length > 0;
+  }
+
+  getNode(name: string) : Node<T> | undefined {
+    return this.nodes.find((node) => node.name === name);
+  }
+
+  addEdge(v: string, w: string) {
+    if (this.hasNode(v) && this.hasNode(w)) {
+      const nodeV = this.getNode(v);
+      const nodeW = this.getNode(w);
+      nodeV.addEdge(nodeW);
+      nodeW.addEdge(nodeV);
+    }
+  }
+
+  printGraph() {
+    // iterate over the vertices
+    for (let i = 0; i < this.nodes.length; i++) {
+      const node = this.nodes[i];
+      let adjNodes = '';
+
+      for (let j = 0; j < node.adjNodes.length; j++) {
+        const adjNode = node.adjNodes[j];
+        adjNodes += `${adjNode.name} `;
+      }
+      // eslint-disable-next-line no-console
+      console.log(`${node.name} -> ${adjNodes}`.trim());
+    }
+  }
+}
+
+export default Graph;
