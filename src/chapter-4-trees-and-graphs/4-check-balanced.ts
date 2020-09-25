4caad6701eb1cc1d590fd62ac97a797fea533c1f
Can Rozanes <37912128+canrozanes@users.noreply.github.com>
2020-09-25 15:30:21 -0400

    add improved solution to c4q4 check balanced

--- src/chapter-4-trees-and-graphs/4-check-balanced.ts
+++ src/chapter-4-trees-and-graphs/4-check-balanced.ts
@@ -1,22 +1,59 @@
 import Node from '../data-structures/binary-search-tree/node';
+import MinimalTree from './2-minimal-tree';
 
-function getHeight<T>(root: Node<T>): number {
-  if (root !== undefined) {
-    return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+class BSTWithCheckBalanced<T> extends MinimalTree<T> {
+  static constructFromArray<T>(array: T[] = []): BSTWithCheckBalanced<T> {
+    const newTree = new BSTWithCheckBalanced<T>();
+
+    newTree.root = newTree.constructMinimalTree(array);
+
+    return newTree;
   }
-  return 0;
+
+  getHeight(root: Node<T>): number {
+    if (root !== undefined && root !== null) {
+      return (
+        Math.max(this.getHeight(root.left), this.getHeight(root.right)) + 1
+      );
+    }
+    return 0;
+  }
+
+  isBalanced(root: Node<T> = this.root): boolean {
+    if (!root) {
+      return true;
+    }
+
+    const heightDiff = Math.abs(
+      this.getHeight(root.left) - this.getHeight(root.right)
+    );
+
+    if (heightDiff > 1) {
+      return false;
+    }
+
+    return this.isBalanced(root.left) && this.isBalanced(root.right);
+  }
+
+  isBalancedImproved(): boolean {
+    return this.checkHeight(this.root) !== Number.MIN_SAFE_INTEGER;
+  }
+
+  checkHeight(root: Node<T>): number {
+    if (!root) return -1;
+
+    const leftHeight = this.checkHeight(root.left);
+    if (leftHeight === Number.MIN_SAFE_INTEGER) return Number.MIN_SAFE_INTEGER;
+
+    const rightHeight = this.checkHeight(root.right);
+    if (rightHeight === Number.MIN_SAFE_INTEGER) return Number.MIN_SAFE_INTEGER;
+
+    const heightDiff = Math.abs(leftHeight - rightHeight);
+
+    if (heightDiff > 1) return Number.MIN_SAFE_INTEGER;
+
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
 }
 
-function isBalanced<T>(root: Node<T>): boolean {
-  if (root !== undefined) {
-    return true;
-  }
-  const heightDiff = Math.abs(getHeight(root.right) - getHeight(root.left));
-
-  if (heightDiff > 1) {
-    return false;
-  }
-  return isBalanced(root.left) && isBalanced(root.right);
-}
-
-export default isBalanced;
+export default BSTWithCheckBalanced;
